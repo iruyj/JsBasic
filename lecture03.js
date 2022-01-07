9a4fd579525389fbf68beecdd7931cb51abe0867
Jyuri <jeon1310@gmail.com>
2022-01-07 14:10:19 +0900

    part 2. 변수

---
+++ lecture03.js
@@ -0,0 +1,8 @@
+// var msg;
+// msg = "Hello World";
+// alert(msg);       // 메세지 출력용
+// console.log(msg);
+
+// prompt : 입력받기
+var name = prompt("이름을 입력해 주세요.");     
+console.log(name, " 님 환영합니다.");
