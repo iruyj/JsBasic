bf8c4d43a66a2398d9b81f7850a080a7931e59e9
Jyuri <jeon1310@gmail.com>
2022-01-08 17:52:19 +0900

    part 3. 기본자료형

---
+++ lecture04.js
@@ -0,0 +1,8 @@
+var a = 100, b = 3.14;
+var c = "안녕하세요", d = "a";
+var e = true, f = false;
+
+// typeof : 매개변수로 들어온 변수의 타입을 반환
+console.log(a, typeof(a));
+console.log(c, typeof(c));
+console.log(e, typeof(e));
