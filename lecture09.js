836d41b0bf3c5b4e2aaa7056e0afebb97c27b062
Jyuri <jeon1310@gmail.com>
2022-01-14 15:30:10 +0900

    part 4. 산술연산자

---
+++ lecture09.js
@@ -0,0 +1,25 @@
+/*
+이항연산자 : A 연산자 B
+    + 더하기
+    - 빼기
+    * 곱하기
+    / 나누기
+    % 나머지
+
+단항연산자 : 연산자A
+    증감연산자 (++,--)
+*/
+var a = 1;
+
+console.log(a);;
+// console.log(a + 1);
+// console.log(++a);
+// console.log(--a);
+console.log(a++);
+console.log(a);
+
+/*
+Math.pow(2, 3); => 8
+Math.sqrt(16); => 4
+Math.random(); => 호출 시 0 ~ 1 사이의 임의의 숫자를 나타냄
+*/
