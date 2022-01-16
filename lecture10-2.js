93ea8a11ff3ef5db855e5c3370545224ad18e7e9
Jyuri <jeon1310@gmail.com>
2022-01-16 15:58:10 +0900

    part 4. 연산자우선순위

---
+++ lecture10-2.js
@@ -0,0 +1,18 @@
+2 * 3 > 4 + 5 && 6 / 3 == 2 || !false
+
+height >= 180 && gender == "male" || height >= 165 && gender == "female"
+
+(height >= 180 && gender == "male") || (height >= 165 && gender == "female")
+
+
+/*
+연산자 우선순위
+1. ++, --
+2. !
+3. *, /, %
+4. +, -
+5. <, <=, >, >=
+6. ==, !=
+7. &&
+8. ||
+*/
